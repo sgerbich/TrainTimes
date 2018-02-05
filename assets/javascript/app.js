5065aedc9ee8616f95cbe0632d02a98b0cc07807
Sean Gerbich <sdgerbich@gmail.com>
2018-02-04 19:10:47 -0500

    working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,23 +14,48 @@ var dest;
 var firstT;
 var freq;
 $(document).ready(function () {
-    
+
     $(document).on("click", "#submitButton", function (event) {
-        
-            event.preventDefault();
-            tName = $("#train-submit").val().trim();
-            dest = $("#dest-submit").val().trim();
-            freq = $("#freq-submit").val().trim();
-            firstT = $("#firstT-submit").val().trim();
-            database.ref().push({
-                tName: tName,
-                dest: dest,
-                freq: freq,
-                firstT: firstT
-            });
+
+        event.preventDefault();
+        tName = $("#train-submit").val().trim();
+        dest = $("#dest-submit").val().trim();
+        freq = $("#freq-submit").val().trim();
+        firstT = $("#firstT-submit").val().trim();
+        database.ref().push({
+            tName: tName,
+            dest: dest,
+            freq: freq,
+            firstT: firstT
+        });
+        $("#train-submit").val("");
+        $("#dest-submit").val("");
+        $("#freq-submit").val("");
+        $("#firstT-submit").val("");
+    })
+    database.ref().on("child_added", function (cSnapshot, prevChildKey) {
+        var trainName = cSnapshot.val().tName;
+        var destination = cSnapshot.val().dest;
+        var frequency = cSnapshot.val().freq;
+        var firstTrain = cSnapshot.val().firstT;
+
+        var trainConv = moment(firstTrain, "hh:mm");
+        var currentTime = moment();
+        var diff = moment().diff(moment(trainConv), "minutes");
+        var remain = diff % frequency;
+        var tilTrain = frequency - remain;
+        var nextTrain = moment().add(tilTrain, "minutes");
+        var reformatTrain = moment(nextTrain).format("hh:mm");
 
 
-        
+        $("#target").append(
+            "<tr><td>" + trainName +
+            "</td><td>" + destination +
+            "</td><td>" + frequency +
+            "</td><td>" + tilTrain +
+            "</td><td>" + reformatTrain +
+            "</td><td>"
+        )
 
     })
 });
